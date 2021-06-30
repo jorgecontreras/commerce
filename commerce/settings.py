a5df3cfa86a779229cf19e3c102193cfab223c7d
Jorge <jrcr23@gmail.com>
2021-06-30 13:46:57 -0400

    change static url

--- commerce/settings.py
+++ commerce/settings.py
@@ -120,6 +120,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://storage.googleapis.com/gifted-commerce/static/'
 
 STATIC_ROOT = 'static/'
